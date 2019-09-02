c85825bafb00c63955b6b0dec6838db51d9f0cac
Matthew Edwards <mtedwards@protonmail.com>
2019-09-02 12:35:53 +0100

    removed vignettes and changed data to new_data in all functions

--- R/effect.R
+++ R/effect.R
@@ -14,45 +14,45 @@ feature_seq <- function(feature, len = 100) {
 
 #' Feature Replace
 #'
-#' @param data data
+#' @param new_data new_data
 #' @param feature_name feature_name
 #' @param feature_value feature_value
 #'
 #' @return
-feature_replace <- function(data, feature_name, feature_value) {
-  dplyr::mutate(data, !!ensym(feature_name) := feature_value)
+feature_replace <- function(new_data, feature_name, feature_value) {
+  dplyr::mutate(new_data, !!ensym(feature_name) := feature_value)
 }
 
 #' Mean Predict
 #'
 #' @param object object
-#' @param data data
+#' @param new_data new_data
 #'
 #' @return
-mean_predict <- function(object, data) {
+mean_predict <- function(object, new_data) {
   if (object$spec$mode == "regression") {
-    parsnip::predict.model_fit(object, data) %>%
+    parsnip::predict.model_fit(object, new_data) %>%
       dplyr::summarise(.mean_pred = mean(.pred))
   } else if (object$spec$mode == "classification") {
-    predict(object, data, type = "prob") %>%
+    predict(object, new_data, type = "prob") %>%
       dplyr::summarise(.mean_pred = mean(.pred_1))
   }
 }
 
-#' Effect
+#' Effect Data
 #'
 #' @param object object
-#' @param data data
+#' @param new_data new_data
 #' @param feature_name feature_name
 #'
 #' @return
 #' @export
 #'
 #' @examples
-effect <- function(object, data, feature_name) {
+effect_data <- function(object, new_data, feature_name) {
   feature_name <- ensym(feature_name)
-  fseq <- feature_seq(dplyr::pull(data, !!feature_name))
-  purrr::map(fseq, feature_replace, data = data, feature_name = !!feature_name) %>%
+  fseq <- feature_seq(dplyr::pull(new_data, !!feature_name))
+  purrr::map(fseq, feature_replace, new_data = new_data, feature_name = !!feature_name) %>%
     purrr::map_dfr(mean_predict, object = object) %>%
     dplyr::bind_cols(!!feature_name := fseq)
 }
@@ -60,32 +60,32 @@ effect <- function(object, data, feature_name) {
 #' Effect Plot
 #'
 #' @param object object
-#' @param data data
+#' @param new_data new_data
 #' @param feature_name feature_name
 #'
 #' @return
 #' @export
 #'
 #' @examples
-effect_plot <- function(object, data, feature_name) {
+effect_plot <- function(object, new_data, feature_name) {
   feature_name <- ensym(feature_name)
-  feature_class <- class(dplyr::pull(data, !!feature_name))
-  effect_data <- effect(object, data, !!feature_name)
+  feature_class <- class(dplyr::pull(new_data, !!feature_name))
+  effect_tbl <- effect_data(object, new_data, !!feature_name)
   if (feature_class == "numeric" & object$spec$mode == "regression") {
-    ggplot2::ggplot(effect_data) +
+    ggplot2::ggplot(effect_tbl) +
     ggplot2::geom_line(ggplot2::aes(x = !!feature_name, y = .mean_pred)) +
     ggplot2::ylab("Prediction")
   } else if (feature_class == "numeric" & object$spec$mode == "classification") {
-    ggplot2::ggplot(effect_data) +
+    ggplot2::ggplot(effect_tbl) +
     ggplot2::geom_line(ggplot2::aes(x = !!feature_name, y = .mean_pred)) +
     ggplot2::ylab("Prediction") +
     ggplot2::ylim(0, 1)
   } else if (feature_class == "factor" & object$spec$mode == "regression") {
-    ggplot2::ggplot(effect_data) +
+    ggplot2::ggplot(effect_tbl) +
     ggplot2::geom_bar(ggplot2::aes(x = !!feature_name, weight = .mean_pred)) +
     ggplot2::ylab("Prediction")
   } else if (feature_class == "factor" & object$spec$mode == "classification") {
-    ggplot2::ggplot(effect_data) +
+    ggplot2::ggplot(effect_tbl) +
     ggplot2::geom_bar(ggplot2::aes(x = !!feature_name, weight = .mean_pred)) +
     ggplot2::ylab("Prediction") +
     ggplot2::ylim(0, 1)
--- R/importance.R
+++ R/importance.R
@@ -1,49 +1,49 @@
 #' Feature Permutation
 #'
-#' @param data data
+#' @param new_data new_data
 #' @param feature_name feature_name
 #'
 #' @return
-feature_permutation <- function(data, feature_name) {
+feature_permutation <- function(new_data, feature_name) {
   feature_name <- ensym(feature_name)
-  dplyr::mutate(data, !!feature_name := sample(!!feature_name))
+  dplyr::mutate(new_data, !!feature_name := sample(!!feature_name))
 }
 
 #' Object Metric
 #'
 #' @param object object
-#' @param data data
+#' @param new_data new_data
 #' @param target_name target_name
 #'
 #' @return
-object_metric <- function(object, data, target_name) {
+object_metric <- function(object, new_data, target_name) {
   target_name <- ensym(target_name)
   if (object$spec$mode == "regression") {
-    parsnip::predict.model_fit(object, data) %>%
-      dplyr::bind_cols(data) %>%
+    parsnip::predict.model_fit(object, new_data) %>%
+      dplyr::bind_cols(new_data) %>%
       yardstick::rmse(truth = !!target_name, estimate = .pred)
   } else if (object$spec$mode == "classification") {
-    parsnip::predict.model_fit(object, data) %>%
-      dplyr::bind_cols(data) %>%
+    parsnip::predict.model_fit(object, new_data) %>%
+      dplyr::bind_cols(new_data) %>%
       yardstick::accuracy(truth = !!target_name, estimate = .pred_class)
   }
 }
 
-#' Importance
+#' Importance Data
 #'
 #' @param object object
-#' @param data data
+#' @param new_data new_data
 #' @param target_name target_name
 #'
 #' @return
 #' @export
 #'
 #' @examples
-importance <- function(object, data, target_name) {
+importance_data <- function(object, new_data, target_name) {
   target_name <- ensym(target_name)
-  metric <- object_metric(object, data, !!target_name)
-  feature_names <- names(dplyr::select(data, -!!target_name))
-  purrr::map(feature_names, feature_permutation, data = data) %>%
+  metric <- object_metric(object, new_data, !!target_name)
+  feature_names <- names(dplyr::select(new_data, -!!target_name))
+  purrr::map(feature_names, feature_permutation, new_data = new_data) %>%
     purrr::map_dfr(object_metric, object = object, target_name = !!target_name) %>%
     dplyr::bind_cols(feature = feature_names) %>%
     dplyr::mutate(.estimate = abs(.estimate - metric$.estimate))
@@ -52,17 +52,18 @@ importance <- function(object, data, target_name) {
 #' Importance Plot
 #'
 #' @param object object
-#' @param data data
+#' @param new_data new_data
 #' @param target_name target_name
 #'
 #' @return
 #' @export
 #'
 #' @examples
-importance_plot <- function(object, data, target_name) {
-  importance(object, data, !!ensym(target_name)) %>%
+importance_plot <- function(object, new_data, target_name) {
+  importance_data(object, new_data, !!ensym(target_name)) %>%
     ggplot2::ggplot() +
     ggplot2::geom_bar(ggplot2::aes(x = forcats::fct_reorder(feature, .estimate, .desc = TRUE), weight = .estimate)) +
+    ggplot2::coord_flip() +
     ggplot2::xlab("Features") +
     ggplot2::ylab(dplyr::if_else(object$spec$mode == "regression", "Root Mean Square Error Drop", "Accuracy Drop"))
 }
