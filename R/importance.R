4afbc9df7c00111d6b406cb9574bea34b9daa3a9
Matthew Edwards <mtedwards@protonmail.com>
2019-09-05 17:07:31 +0100

    added yarkstick metrics to permutation options

--- R/importance.R
+++ R/importance.R
@@ -1,74 +1,69 @@
-#' Feature Permutation
+#' Permute Feature
 #'
-#' @param new_data A rectangular data object, such as a data frame.
-#' @param feature_name A feature name.
-#'
-#' @return
-feature_permutation <- function(new_data, feature_name) {
-  feature_name <- ensym(feature_name)
-  dplyr::mutate(new_data, !!feature_name := sample(!!feature_name))
-}
-
-#' Object Metric
-#'
-#' @param object An object of class model_fit.
-#' @param new_data A rectangular data object, such as a data frame.
+#' @param data A rectangular data object, such as a data frame.
+#' @param feature A feature variable.
 #'
 #' @return
-object_metric <- function(object, new_data) {
-  pred_data <- parsnip::predict.model_fit(object, new_data) %>%
-    dplyr::bind_cols(new_data)
-  if (object$spec$mode == "regression") {
-    yardstick::mae(pred_data, truth = !!sym(object$preproc$y_var), estimate = .pred)
-  } else if (object$spec$mode == "classification") {
-    yardstick::accuracy(pred_data, truth = !!sym(object$preproc$y_var), estimate = .pred_class)
-  } else {
-    stop("Invalid `object` mode")
-  }
+permute_feature <- function(data, feature) {
+  dplyr::mutate(data, {{ feature }} := sample(!!sym(feature)))
 }
 
-#' Importance Data
+#' Estimate Metric
 #'
-#' The importance_data function generates a data frame of permutation importance
-#' values for each of the features in new_data.
+#' @param object An object of class `model_fit`.
+#' @param data A rectangular data object, such as a data frame.
+#' @param target The target variable.
+#' @param metric A metric funtion from the `yardstick` package.
 #'
-#' @param object An object of class model_fit.
-#' @param new_data A rectangular data object, such as a data frame.
+#' @return
+estimate_metric <- function(object, data, target, metric) {
+  parsnip::predict.model_fit(object, data) %>%
+    dplyr::bind_cols(data) %>%
+    metric(truth = {{ target }}, estimate = dplyr::first(.)) %>%
+    dplyr::select(.metric, .estimate)
+}
+
+#' Estimate Importance
+#'
+#' @param object An object of class `model_fit`.
+#' @param data A rectangular data object, such as a data frame.
+#' @param target The target variable.
+#' @param metric A metric funtion from the `yardstick` package.
 #'
 #' @return
 #' @export
 #'
 #' @examples
-importance_data <- function(object, new_data) {
-  metric <- object_metric(object, new_data)
-  feature_names <- names(dplyr::select(new_data, -!!sym(object$preproc$y_var)))
-  purrr::map(feature_names, feature_permutation, new_data = new_data) %>%
-    purrr::map_dfr(object_metric, object = object) %>%
-    dplyr::bind_cols(.feature = feature_names) %>%
-    dplyr::mutate(.estimate = abs(.estimate - metric$.estimate))
+estimate_importance <- function(object, data, target, metric) {
+  metric_data <- estimate_metric(object, data, {{ target }}, metric)
+  features <- names(dplyr::select(data, -{{ target }}))
+  purrr::map(features, permute_feature, data = data) %>%
+    purrr::map_dfr(estimate_metric, object = object, target = {{ target }}, metric = metric) %>%
+    dplyr::bind_cols(.feature = features) %>%
+    dplyr::mutate(.importance = abs(.estimate - metric_data$.estimate)) %>%
+    dplyr::select(.metric, .feature, .importance)
 }
 
-#' Importance Plot
-#'
-#' The importance_plot function generates a plot of permutation importance
-#' values for each of the features in new_data.
+#' Plot Importance
 #'
 #' @param object An object of class model_fit.
-#' @param new_data A rectangular data object, such as a data frame.
+#' @param data A rectangular data object, such as a data frame.
+#' @param target The target variable.
+#' @param metric A metric funtion from the `yardstick` package.
 #' @param title A character string for the title.
-#' @param subtitle A character string for the subtitle displayed under the title.
 #'
 #' @return
 #' @export
 #'
 #' @examples
-importance_plot <- function(object, new_data,  title = "Permutation Importance Plot") {
-  importance_data(object, new_data) %>%
-    ggplot2::ggplot() +
-    ggplot2::geom_bar(ggplot2::aes(x = forcats::fct_reorder(.feature, .estimate), weight = .estimate)) +
+plot_importance <- function(object, data, target, metric, title = "Permutation Importance Plot") {
+  importance_data <- estimate_importance(object, data, {{ target }}, metric)
+  metric_name <- importance_data$.metric[[1]]
+  ggplot2::ggplot(importance_data) +
+    ggplot2::geom_bar(ggplot2::aes(x = forcats::fct_reorder(.feature, .importance), weight = .importance)) +
     ggplot2::coord_flip() +
     ggplot2::labs(title = title) +
-    ggplot2::xlab("Features") +
-    ggplot2::ylab(dplyr::if_else(object$spec$mode == "regression", "Importance (MAE)", "Importance (accuracy)")) +
+    ggplot2::xlab("Feature") +
+    ggplot2::ylab(glue::glue('Importance ({ metric_name })')) +
     ggplot2::theme_grey()
 }
